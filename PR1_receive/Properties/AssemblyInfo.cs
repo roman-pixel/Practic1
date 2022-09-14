f1a53c213193d8cf341fd1eafef5e26e68c395e1
Roman Sadov <romansrgvch@outlook.com>
2022-09-14 20:12:34 +0300

    Добавьте файлы проекта.

---
+++ PR1_receive/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR1_receive")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR1_receive")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("857160d6-3f6b-41af-89e6-d73f6ef9f0fc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practic1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Practic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //string message = "Hello World!";
+                Console.WriteLine("Enter your message:");
+                var message = Console.ReadLine();
+
+                int Num;
+                bool isNum = int.TryParse(message, out Num);
+
+                var queueChoice = "";
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+
+                    if (isNum)
+                    {
+                        queueChoice = "num";
+                    }
+                    else
+                    {
+                        queueChoice = "notNum";
+                    }
+
+                    channel.QueueDeclare(queue: queueChoice,
+                                                         durable: false,
+                                                         exclusive: false,
+                                                         autoDelete: false,
+                                                         arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queueChoice,
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+            }
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+    }
+}
